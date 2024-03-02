4bfb33cc4d82feffac0655c272c58f6074951949
Jorjikia Beka <jorjikiabeka@Jorjikias-MacBook-Air.local>
2024-03-02 14:25:54 +0400

    adding multi user model

---
+++ user/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = 'ADMIN', 'Admin'
+        VENDOR = 'VENDOR', 'Vendor'
+        CONSUMER = 'CONSUMER', 'Consumer'
+
+    base_role = Role.ADMIN
+    role = models.CharField(max_length = 50, choices = Role.choices)
+
+    def save(self,  *args, **kwargs):
+        if not self.pk: 
+            self.role = self.base_role
+            return super().save(*args, **kwargs)
+
+class ConsumerManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role = User.Role.CONSUMER)
+
+class Consumer(User):
+    base_role = User.Role.CONSUMER
+    consumer = ConsumerManager()
+    class Meta:
+        proxy = True
+    
+    def welcome(self):
+        return "Only for consumers"
+    
+class VendorManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role = User.Role.VENDOR)
+
+class Vendor(User):
+    base_role = User.Role.VENDOR
+    vendor = VendorManager()
+    class Meta:
+        proxy = True
+    
+    def welcome(self):
+        return "Only for vendors"
