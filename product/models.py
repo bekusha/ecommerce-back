741848eeec206581036910d088f784b386a39471
Jorjikia Beka <jorjikiabeka@Jorjikias-MacBook-Air.local>
2024-03-10 16:58:37 +0400

    correcting categories field in products model

--- product/models.py
+++ product/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from user.models import User
 
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, related_name='children')
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     vendor = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'role': User.Role.VENDOR})
     name = models.CharField(max_length=255)
@@ -11,8 +21,8 @@ class Product(models.Model):
     image3 = models.ImageField(upload_to='product_images/', blank=True, null=True)
     image4 = models.ImageField(upload_to='product_images/', blank=True, null=True)
     image5 = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)  # Updated to use ForeignKey
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
-    
--- server/urls.py
+++ server/urls.py
@@ -1,11 +1,14 @@
-
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings  # Import settings
+from django.conf.urls.static import static  # Import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
- 
-
+    # Your other paths here
 ]
+
+# Serving media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
