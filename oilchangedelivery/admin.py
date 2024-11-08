dc97ade630607bc059c20f351c41d92fdf6e9fd6
Jorjikia Beka <jorjikiabeka@Jorjikias-MacBook-Air.local>
2024-11-08 23:43:27 +0400

    fixing product issue in changeddelivery model

--- oilchangedelivery/admin.py
+++ oilchangedelivery/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import OilChangeDelivery
 
 class OilChangeDeliveryAdmin(admin.ModelAdmin):
-    fields = ('user', 'phone', 'address', 'email', 'product', 'ordered_at',)
+    fields = ('user', 'phone', 'address', 'email',  'ordered_at',)
     readonly_fields = ('ordered_at',)
 admin.site.register(OilChangeDelivery, OilChangeDeliveryAdmin)
