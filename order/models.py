90ebd0206f07e6466ac1e33a6a3f6654c3041f8a
Jorjikia Beka <jorjikiabeka@Jorjikias-MacBook-Air.local>
2024-11-27 13:47:40 +0400

    adding product as null true in order

--- order/models.py
+++ order/models.py
@@ -37,7 +37,7 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items', blank=True, null=True)
     quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
